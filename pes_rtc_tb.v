6c54b5e6f69fa8cb49e1c2e72e3fb7cc25519bf4
ramdev604 <43489027+ramdev604@users.noreply.github.com>
2023-09-01 14:25:18 +0530

    Update pes_rtc_tb.v

--- pes_rtc_tb.v
+++ pes_rtc_tb.v
@@ -2,7 +2,7 @@
 module pes_rtc_tb;
 reg clk,rst;
 wire [3:0]hrm,hrl,minm,minl,secm,secl;
-  iiitb_rtc r1(.clkin(clk),.rst(rst),.hrm(hrm),.hrl(hrl),.minm(minm),.minl(minl),.secm(secm),.secl(secl)); 
+  pes_rtc r1(.clkin(clk),.rst(rst),.hrm(hrm),.hrl(hrl),.minm(minm),.minl(minl),.secm(secm),.secl(secl)); 
 initial begin
 clk=0;rst=0;
 $dumpfile("iiitb_rtc_out.vcd");
